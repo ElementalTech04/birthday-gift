0aaf16084211839b61605c75de8670295cb2cdd0
Frankie Rodriguez <rfrankie@gmail.com>
2025-02-18 21:22:59 -0500

    origins

--- API/app/main.py
+++ API/app/main.py
@@ -7,10 +7,15 @@ app = FastAPI(title="Birthday API")
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # In production, replace with specific origins
+    allow_origins=[
+        "https://birthday.symphony-ts.com",
+        "http://birthday.symphony-ts.com",  # If you need HTTP during testing
+        "http://localhost:3000",  # For local development
+    ],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["Content-Type", "Authorization", "Accept"],
+    expose_headers=["Content-Type"],
 )
 
 # Include routers
